4e1c7802c32a3ed81ab929d5614695c62563831d
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-08 15:25:12 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  * Description:
  * * This is a c script
  * * It prints an output to the screen
 * without using the printf function
 * Instead, it uses the puts function
-*
+* Return: Always 0
 */
 
 int main(void)
