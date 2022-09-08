b1ee1f03ac1678e1ee03f0721099fed673fd1445
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-08 15:18:54 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**
-* This is a c script 
-* It prints an output to the screen
+/*
+ * * This is a c script
+ * * It prints an output to the screen
 * without using the printf function
 * Instead, it uses the puts function
 *
