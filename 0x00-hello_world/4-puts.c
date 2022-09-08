cdfa2852db6c82c4f2405e891613857693200606
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-08 15:13:29 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
- *This is a c script 
- *It prints an output to the screen
- *without using the printf function
- *Instead, it uses the puts function
- *
- */
+
+/**
+*This is a c script 
+*It prints an output to the screen
+*without using the printf function
+*Instead, it uses the puts function
+*
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
