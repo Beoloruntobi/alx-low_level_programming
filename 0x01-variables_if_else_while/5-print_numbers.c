bf5d07bdd51c5e8a205152633729ee50af1d2ab5
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-11 11:51:39 -0700

    Print single digit numbers from 0 to 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the script
  *
- * Description - This script prints 
+ * Description - This script prints
  * all single digit numbers in base 10
  * starting from 0, followed by a new line.
  *
@@ -13,10 +13,10 @@
 int main(void)
 {
 	int num = 0;
-	
+
 	while (num < 10)
 	{
-		putchar(num);
+		printf("%i", num);
 		num++;
 	}
 	putchar('\n');
