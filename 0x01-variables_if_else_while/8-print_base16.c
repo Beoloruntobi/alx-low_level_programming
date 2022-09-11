0e363cec8b4ddf1bfa935ac355abd2fc665203f8
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-11 12:30:55 -0700

    Hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Print hexadecimal numbers
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 102)
+	{
+		putchar(num);
+
+		if (num == 57)
+		{
+			num += 39;
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
