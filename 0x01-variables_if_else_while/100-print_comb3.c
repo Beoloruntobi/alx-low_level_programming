94f2e89b9988f67c653dae6619ac20a91981cb5e
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-12 06:46:35 -0700

    Combinations of 2 single digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ * Description - print the possible combinations
+ * of 2 single digit numbers
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 < 9; num_1++)
+	{
+		for (num_2 = num_1 + 1; num_2 <= 9; num_2++)
+		{
+			putchar(num_1 + '0');
+			putchar(num_2 + '0');
+			if (num_1 == 8 && num_2 == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
