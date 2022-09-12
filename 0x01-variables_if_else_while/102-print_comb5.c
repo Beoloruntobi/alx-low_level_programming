39b6133dc2f9d61eb8ceebd44c96799509ff4d6c
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-12 07:45:23 -0700

    Two two digit combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ * Description - Print numbers between 00 and 99
+ * Return: Return 0 if successful
+ */
+
+int main(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 < 99; num_1++)
+	{
+		for (num_2 = num_1 + 1; nu_2 <= 99; num_2++)
+		{
+			putchar((num_1 / 10) + '0');
+			putchar((num_1 % 10) + '0');
+			putchar(' ');
+			putchar((num_2 / 10) + '0');
+			putchar((num_2 % 10) + '0');
+			if (num_1 == 98 && num_2 == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
