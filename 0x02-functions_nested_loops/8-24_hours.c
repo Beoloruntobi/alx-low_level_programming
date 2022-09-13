bbc43b084790e2ef83136ad1ce579cd88deddc66
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-13 11:45:44 -0700

    Print from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - Starts here
- * Description - This script prints from 
+ * Description - This script prints from
  * 00:00 to 23:59
  * Return: Return 0 at 23:59
  */
@@ -11,7 +11,7 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a < 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -25,11 +25,23 @@ void jack_bauer(void)
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
-					if (a == 2 && b == 3 && c == 5 && d == 9)
-					{
-						break;
-					}
 				}
+			}
+		}
+	}
+	a = 2;
+	for (b = 0; b <= 3; b++)
+	{
+		for (c = 0; c <= 5; c++)
+		{
+			for (d = 0; d <= 9; d++)
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+				_putchar(':');
+				_putchar(c + '0');
+				_putchar(d + '0');
+				_putchar('\n');
 			}
 		}
 	}
