0970069647851633d90e766e272110142dba1e1e
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-13 09:33:35 -0700

    Chech for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,27 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 #include <ctype.h>
 
 /**
- * main - Starts here
+ * _islower - Starts here
  * Description - This script checks that the input is in lowercase
  * Return: Returns 1 if lowercase, 0 otherwise
  */
-
-int _islower(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-
-}
 
 int _islower(int c)
 {
