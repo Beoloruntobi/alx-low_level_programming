3e5ff3227e582ac101035bc3b26e3e7f7ec02f1b
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-13 12:22:59 -0700

    print up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,32 +12,17 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (num = n; num >= 98; num--)
+		for (num = n; num > 98; num--)
 		{
-			_putchar(num + '0');
-			if (num == 98)
-			{
-				continue;
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", num);
 		}
-	}
-	else if (n == 98)
-	{
-		_putchar(n + '0');
 	}
 	else
 	{
-		for (num = n; num <= 98; num++)
+		for (num = n; num < 98; num++)
 		{
-			putchar(num + '0');
-			if (num == 98)
-			{
-				continue;
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", num);
 		}
 	}
+	printf("98\n");
 }
