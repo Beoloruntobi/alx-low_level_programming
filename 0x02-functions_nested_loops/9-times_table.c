328f6971c64ef62e1cbc2c62d46e50c1e3f5823e
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-14 12:30:14 -0700

    Multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - Function to create multiplication table
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d", j*j);
+			if (j == 9)
+			{
+				printf("$");
+				break;
+			}
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
