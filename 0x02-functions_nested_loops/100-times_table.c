aaacac32ec88174944059f0a2f44b5df937f425d
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-14 12:57:20 -0700

    N times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - Function to create multiplication table
+ * @n: Input into function
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 15 || n < 0)
+	{
+		break;
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			printf("%d", 0);
+			
+			for (j = 1; j <= n; j++)
+			{
+				printf(", ");
+				if (i * j <= 9)
+				{
+					printf(" ");
+				}
+				else
+				{
+					printf("%d", (i * j) / 10);
+				}
+				printf("%d", (i * j) % 10);
+				if (j == n)
+				{
+					break;
+				}
+			}
+			printf("\n");
+		}
+	}
+}
