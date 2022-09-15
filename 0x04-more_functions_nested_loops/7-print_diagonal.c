653abbd9c66fa215ec7bf19f46b2437c2bab964c
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-15 13:37:48 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print a line
+ * @n: number of underscores to make up the line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('\');
+		}
+	}
+	_putchar('\n');
+}
