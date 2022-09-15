b3fa7cf2030c627057184533a0ea6999310ce40b
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-15 14:13:57 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,19 +9,12 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	for (i = 1; i <= size; i++)
 	{
-		for (i = 1; i <= size; i++)
+		for (j = 1; j <= size; j++)
 		{
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
-	}
-	else
-	{
 		_putchar('\n');
 	}
 }
