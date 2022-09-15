5c2f91078fdfc49a79987d9dd26894212dd859c0
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-15 12:04:01 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplication function
+ * @a: First input
+ * @b: Second input
+ * Return: Returns the multiple
+ */
+
+int mul(int a, int b)
+{
+	int multip;
+
+	multip = a * b;
+
+	return (multip);
+}
