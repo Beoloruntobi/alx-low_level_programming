05841c82dbcd916903736b83ab058c577eb785d0
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-19 12:24:51 -0700

    Reverse string but don't print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Function to reverse string
+ * @s: String to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char str[];
+
+	str = strrev(s);
+}
