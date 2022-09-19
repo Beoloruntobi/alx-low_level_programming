1c2194400c04f670c9c8bb874a9ebe70eda92c15
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-19 13:44:03 -0700

    N numbers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,11 @@
 
 void print_array(int *a, int n)
 {
-	int len, i;
+	int i;
 
-	len = strlen(a);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
-	printf("%d\n", a[n]);
+	printf("%d\n", a[n - 1]);
 }
