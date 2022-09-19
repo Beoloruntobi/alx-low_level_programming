aa8986b4e68f97586c470e677118f9fca9935c73
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-19 12:03:47 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: String to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
+
