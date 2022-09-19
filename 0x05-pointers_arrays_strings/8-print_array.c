28cfe49cf094a562f63fee3ff3a35c04d5122752
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-19 13:51:33 -0700

    Print n number of elements in array a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,16 +3,24 @@
 
 /**
  * print_array - Print the second half of a string
- * @str: Input string
+ * @a: Input array
+ * @n: number of numbers in array to print
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (n >= 0)
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d\n", a[n - 1]);
 	}
-	printf("%d\n", a[n - 1]);
+	else
+	{
+		printf("\n");
+	}
 }
