1e6ffd97565677dcd4e26fc5fb6c7e91b9e63942
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-09-29 16:01:00 -0700

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * prime_ - Function checks if number is prime
+ * @n: number
+ * @i: factor
+ * Return: Returns 1 if prime, else, 0
+ */
+
+int prime_(int n, int i)
+{
+	if (n % i) == 0)
+	{
+		if (n == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0 + prime_(n, i + 1));
+}
+/**
+ * is_prime_number - returns 1 if prime, else, 0
+ * @n: number
+ * Return: Returns 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (0);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (prime_(n, 2));
+}
