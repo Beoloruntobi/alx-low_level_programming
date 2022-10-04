cd7524eaf0960a648c2cb82d5af1fcee2497a440
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-10-04 13:48:13 -0700

    What's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program prints its name
+ *
+ * @argc: holds arguments passed
+ * @argv: array pointer that holds the arguments
+ *
+ * Return: 0 if right
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
