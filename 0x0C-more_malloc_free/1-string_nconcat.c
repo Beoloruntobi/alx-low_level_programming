16551785b4f4ea1be654f8c1b556ce1135bfe987
Beoloruntobi <beoloruntobi@yahoo.co.uk>
2022-10-06 14:42:05 -0700

    CONCATENATE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,38 +14,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	l1 = 0;
 	l2 = 0;
 
 	while (s1[l1] != '\0')
-	{
 		l1++;
-	}
 	while (s2[l2] != '\0')
-	{
 		l2++;
-	}
 
 	if (n >= 12)
-	{
 		n = 12;
-	}
 	str = (char *) malloc((l1 + n + 1) * sizeof(char));
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		str[i] = s1[i];
-	}
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
 		str[i] = s2[j];
